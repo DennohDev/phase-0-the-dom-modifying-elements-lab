e05c4714f0b412f049b334cb1cb00da99b98f361
Dennis Maina <denniskamwene1@gmail.com>
2023-09-17 18:34:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const elementToRemove = document.getElementById("main");
+
+if (elementToRemove) {
+    elementToRemove.remove();
+} else {
+    console.log("Element not found.");
+}
+
+const newHeader = document.getElementById('victory');
+
+  if (newHeader) {
+    newHeader.textContent = 'Your Name is the champion';
+  }
